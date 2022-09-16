58b374ec8bccd83fe1be44d61df082603d7738bb
ZanYge <ZanYge@qq.com>
2022-09-16 11:44:44 +0800

    最后一次修改

--- src/main/java/com/it/test/Pet.java
+++ src/main/java/com/it/test/Pet.java
@@ -5,5 +5,6 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 public class Pet {
     public static void main(String[] args) {
         System.out.println("你好世界");
+        System.out.println("你好世界");
     }
 }
