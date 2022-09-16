a5b6cbef0978fd78dc5fc4b0b30f8d745a6a0108
张子淇 <zhangadmin0721@163.com>
2022-09-16 03:37:01 +0000

    update src/main/java/com/it/test/Pet.java. 你好世界
    Signed-off-by: 张子淇 <zhangadmin0721@163.com>

--- src/main/java/com/it/test/Pet.java
+++ src/main/java/com/it/test/Pet.java
@@ -1,13 +1,9 @@
-﻿package com.it.test;
+package com.it.test;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Pet {
     public static void main(String[] args) {
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
+        System.out.println("你好世界");
     }
 }
